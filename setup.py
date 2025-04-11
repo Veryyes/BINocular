13ba99e5f9cb657590ab9256e89c14fb8950684a
brandon <bwong7@gmu.edu>
2025-04-10 20:21:31 -0400

    added pre-commit hooks

--- setup.py
+++ setup.py
@@ -5,32 +5,38 @@ setup(
     version="1.1",
     description="Common Binary Framework",
     author="Brandon Wong",
-    packages = ["binocular"],
-    install_requires = [
-        'rich',
-        'typer',
-        'pydantic',
-        'networkx',
-        'SQLAlchemy',
-        'coloredlogs',
-        'archinfo',
-        'pyvex',
-        'GitPython',
-        'meson',
-        'rzpipe',
-        'tree-sitter',
-        'tree-sitter-c',
-        'requests',
-        'IPython',
-        'sphinx'
+    packages=["binocular"],
+    install_requires=[
+        "rich",
+        "typer",
+        "pydantic",
+        "networkx",
+        "SQLAlchemy",
+        "coloredlogs",
+        "archinfo",
+        "pyvex",
+        "GitPython",
+        "meson",
+        "rzpipe",
+        "tree-sitter",
+        "tree-sitter-c",
+        "requests",
+        "IPython",
+        "sphinx",
     ],
-    extras_require = {
-        'dev': ['pytest', 'sphinx', 'sphinx_rtd_theme', 'sphinx_mdinclude', 'build', 'types-networkx']
+    extras_require={
+        "dev": [
+            "pre-commit",
+            "mypy",
+            "pytest",
+            "sphinx",
+            "sphinx_rtd_theme",
+            "sphinx_mdinclude",
+            "build",
+            "types-networkx",
+            "black",
+        ]
     },
-    entry_points={
-        "console_scripts": ['binocular = binocular.run:main']
-    },
-    package_data={
-        'binocular': ['scripts/*']
-    }
+    entry_points={"console_scripts": ["binocular = binocular.run:main"]},
+    package_data={"binocular": ["scripts/*"]},
 )
